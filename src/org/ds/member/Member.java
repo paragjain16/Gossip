0342593b03cd87fcc064540198dc3f9e58800899
paragjain16 <pjain11@illinois.edu>
2013-09-28 20:15:58 -0500

    Done Gossiper

--- src/org/ds/member/Member.java
+++ src/org/ds/member/Member.java
@@ -1,21 +1,32 @@
 package org.ds.member;
 
 import java.io.Serializable;
+import java.net.InetAddress;
 import java.util.Date;
 
 public class Member implements Serializable{
-	private String address;
+	private InetAddress address;
 	private int heartBeat;
 	private String identifier;
 	private long timeStamp;
+	private int port;
 
-	public Member(String address, String id){
+	public Member(InetAddress address, String id, int port){
 		this.address = address;
 		this.heartBeat = 0;
-		this.identifier = id+address;
+		this.identifier = id+address.getHostAddress();
 		this.timeStamp = new Date().getTime();
+		this.port = port;
 	}
 	
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
 	public long getTimeStamp() {
 		return timeStamp;
 	}
@@ -27,10 +38,10 @@ public class Member implements Serializable{
 		this.setHeartBeat(getHeartBeat()+1);
 	}
 	
-	public String getAddress() {
+	public InetAddress getAddress() {
 		return address;
 	}
-	public void setAddress(String address) {
+	public void setAddress(InetAddress address) {
 		this.address = address;
 	}
 	public int getHeartBeat() {
@@ -45,6 +56,11 @@ public class Member implements Serializable{
 	public void setIdentifier(String identifier) {
 		this.identifier = identifier;
 	}
-
+	public boolean checkTimeOut(){
+		if(new Date().getTime() - this.timeStamp >3){
+			return true;
+		}
+		return false;
+	}
 	
 }
--- src/org/ds/node/Gossiper.java
+++ src/org/ds/node/Gossiper.java
@@ -1,11 +1,17 @@
 package org.ds.node;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Set;
 
+import org.ds.logger.DSLogger;
 import org.ds.member.Member;
 
 public class Gossiper implements Runnable{
@@ -30,14 +36,43 @@ public class Gossiper implements Runnable{
 			itself.setTimeStamp(new Date().getTime());
 			aliveMembers.put(itself.getIdentifier(), itself);
 			Set<String> keys = aliveMembers.keySet();;
+			Member aMember;
 			for(String key: keys){
-				
+				aMember =aliveMembers.get(key);
+				if(aMember.checkTimeOut()){
+					deadMembers.put(aMember.getIdentifier(), aMember);
+					aliveMembers.remove(aMember.getIdentifier());
+				}
 			}
 			memberList = new ArrayList<Member>(aliveMembers.values());
-			
 		}
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = null;
+		try {
+			oos = new ObjectOutputStream(baos);
+			oos.writeObject(memberList);
+			byte[] buf = baos.toByteArray();
+			Member memberToGossip = chooseRandom();
+			if(memberToGossip!=null){
+				DatagramPacket packet = new DatagramPacket(buf, buf.length, memberToGossip.getAddress(), memberToGossip.getPort());
+				socket.send(packet);
+			}
+		}catch (IOException e) {
+			DSLogger.log("Gossiper", "run", e.getMessage());
+		}
+		
 	}
-	public void updateMemberList(){
+	
+	public Member chooseRandom(){
+		Random random = new Random();
+		int tryAnother = 15;
+		while(tryAnother-- >0){
+			int i = random.nextInt(memberList.size());
+			if(!(memberList.get(i) == itself)){
+				return memberList.get(i);
+			}
+		}
+		return null;
 		
 	}
 }
